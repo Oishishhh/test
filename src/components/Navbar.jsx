7811f1e6d54e109d5630d1fa27a894be16a425ec
Oishishhh <shodiyevodil21@gmail.com>
2024-03-11 11:25:32 +0500

    ads

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,10 @@
 import './Navbar.css';
-import  Button  from './Button';
-
 function Navbar () {
     return(
         <div className="Navbar">
             <div className='box'>
                 <h3>Lorem, ipsum dolor.</h3>
-                <h1 className='word'>Free Ecommerce</h1>
-                <h1 className='word2'>Template for Webflow</h1>
-                <Button className="box2"/>
+                <h1>Change</h1>
             </div>
         </div>
     );
